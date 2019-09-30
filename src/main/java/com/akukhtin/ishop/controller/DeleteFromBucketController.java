d7ef61a09b54b6166f92f4c4f823413bdb00a77d
akukhtin <alex.kukhtin.dev@gmail.com>
2019-09-30 10:43:25 +0300

    fixed controller

--- src/main/java/com/akukhtin/ishop/controller/DeleteFromBucketController.java
+++ src/main/java/com/akukhtin/ishop/controller/DeleteFromBucketController.java
@@ -17,7 +17,7 @@ public class DeleteFromBucketController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws IOException {
         Long userId = (Long) req.getSession(true).getAttribute("userId");
         String itemId = req.getParameter("item_id");
         bucketService.deleteItem(bucketService.getByUser(userId).getId(), Long.valueOf(itemId));
