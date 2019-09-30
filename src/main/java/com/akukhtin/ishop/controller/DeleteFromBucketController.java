e740c95389b32631c3b767b85fecc58ededfef8b
akukhtin <alex.kukhtin.dev@gmail.com>
2019-09-30 10:05:05 +0300

    jdbc item impl fixed

--- src/main/java/com/akukhtin/ishop/controller/DeleteFromBucketController.java
+++ src/main/java/com/akukhtin/ishop/controller/DeleteFromBucketController.java
@@ -18,6 +18,7 @@ public class DeleteFromBucketController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
         Long userId = (Long) req.getSession(true).getAttribute("userId");
         String itemId = req.getParameter("item_id");
         bucketService.deleteItem(bucketService.getByUser(userId).getId(), Long.valueOf(itemId));
